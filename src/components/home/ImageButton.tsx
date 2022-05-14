e4d54a6159f7c5a7455856deba9cb3b0c62a05e2
Genadi Tsolov <genadi.tsolov@gmail.com>
2022-05-14 19:56:53 +0300

    fix: place card titile

--- src/components/home/ImageButton.tsx
+++ src/components/home/ImageButton.tsx
@@ -69,6 +69,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   title: {
+    padding: 2.5,
     fontWeight: "bold",
     textAlign: "center",
     color: ColorSchema.dark.text,
